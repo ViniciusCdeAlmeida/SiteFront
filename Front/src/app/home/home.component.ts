dd798898d59de0def0546f165437831ecce43584
ViniciusCdeAlmeida <viniciusc.almeida1@gmail.com>
2018-02-27 20:56:39 -0300

    Contact form OK

--- Front/src/app/home/home.component.ts
+++ Front/src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -6,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  contactForm: FormGroup;
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
+    this.initForm();
+  }
+
+  onSubmit() {
+    console.log(this.contactForm.value)
+  }
+
+  onCancel() {
+    this.router.navigate(['../'], { relativeTo: this.route });
+  }
+
+  private initForm() {
+    let contactName = '';
+    let contactTitle = '';
+    let contactEmail = '';
+    let contactMenssage = '';
+
+    /* ONDE GRAVA NO JSON */
+    this.contactForm = new FormGroup({
+      'name': new FormControl(contactName, Validators.required),
+      'messageSub': new FormControl(contactTitle, Validators.required),
+      'email': new FormControl(contactEmail, Validators.required),
+      'text': new FormControl(contactMenssage, Validators.required)
+    });
   }
 
 }
