bca851c844d5eae51f0085b2c18a6fb07ee7fefc
ViniciusCdeAlmeida <viniciusc.almeida1@gmail.com>
2018-02-28 15:34:03 -0300

    Mailgun Config Done

--- Front/src/app/home/home.component.ts
+++ Front/src/app/home/home.component.ts
@@ -2,24 +2,34 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
+import { StorageService } from '../shared/service/storage.service'
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [StorageService]
 })
 export class HomeComponent implements OnInit {
 
   contactForm: FormGroup;
   constructor(
     private route: ActivatedRoute,
-    private router: Router) { }
+    private router: Router,
+    private storageService: StorageService) { }
 
   ngOnInit() {
     this.initForm();
   }
 
   onSubmit() {
-    console.log(this.contactForm.value)
+    // console.log(this.contactForm.get("messageSub").value)
+    // console.log(this.contactForm.get("email").value)
+    // console.log(this.contactForm.get("text").value)
+    this.storageService.sendEmail(
+      this.contactForm.get("email").value, 
+      this.contactForm.get("messageSub").value, 
+      this.contactForm.get("text").value)
   }
 
   onCancel() {
@@ -27,14 +37,12 @@ export class HomeComponent implements OnInit {
   }
 
   private initForm() {
-    let contactName = '';
     let contactTitle = '';
     let contactEmail = '';
     let contactMenssage = '';
 
     /* ONDE GRAVA NO JSON */
     this.contactForm = new FormGroup({
-      'name': new FormControl(contactName, Validators.required),
       'messageSub': new FormControl(contactTitle, Validators.required),
       'email': new FormControl(contactEmail, Validators.required),
       'text': new FormControl(contactMenssage, Validators.required)
--- Front/src/app/shared/service/storage.service.ts
+++ Front/src/app/shared/service/storage.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Http, Response, Headers } from '@angular/http';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
 
-import {Observable} from 'rxjs/Rx';
+import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/catch';
@@ -14,70 +14,101 @@ import { ArtService } from './art.service'
 @Injectable()
 export class StorageService {
   constructor(
-    private http: Http, 
+    private http: Http,
     private artService: ArtService,
-    private categoryService: CategoryService) {}
+    private categoryService: CategoryService) { }
 
   // private urlArt: string = "http://192.168.182.131:3000/sites";
   // private urlCat: string = "http://192.168.182.131:3000/categories";
   private urlCat: string = "https://vast-woodland-42003.herokuapp.com/categories";
   private urlArt: string = "https://vast-woodland-42003.herokuapp.com/sites";
+  private apiKey: string = "api:key-9fe58fe05edc2b8127804db1b3797a75";
+  private mailgunUrl: string = "sandbox3e756d1080da42c599d84e9026e2a2c9.mailgun.org";
 
-  getArts(){
+  getArts() {
     this.http.get(this.urlArt).map((response: Response) => {
-        const arts: Art[] = response.json(); 
-        return arts;}).subscribe(
-          (art: Art[]) => {this.artService.setArts(art)});
+      const arts: Art[] = response.json();
+      return arts;
+    }).subscribe(
+      (art: Art[]) => { this.artService.setArts(art) });
   }
 
-  getArt(id){
-        return this.http.get(this.urlArt + '/' + id)
-          .map(art => art.json());
+  getArt(id) {
+    return this.http.get(this.urlArt + '/' + id)
+      .map(art => art.json());
   }
 
-  addArt(art){
+  addArt(art) {
     console.log(art)
-    return this.http.post(this.urlArt, {'site': art})
+    return this.http.post(this.urlArt, { 'site': art })
       .map(res => res.json());
   }
 
-  updateArts(art){
-    return this.http.put(this.urlArt + '/' + art.id,  {'site': art})
-      .map(res => res.json());
-  }
-
-  deleteArt(id){
-    return this.http.delete(this.urlArt + '/' + id)
-      .map(res => res.json());
-  }
-
-//--------------------------------------------//
-
-getCategories(){
-  this.http.get(this.urlCat).map((response: Response) => {
-      const cats: Category[] = response.json(); 
-      return cats;}).subscribe(
-        (cat: Category[]) => {this.categoryService.setCategories(cat)});
-}
-
-  getCategory(id){
-        return this.http.get(this.urlCat + '/' + id)
-          .map(category => category.json());
-  }
-
-  addCategory(category){
-    return this.http.post(this.urlCat, {'category': category})
+  updateArts(art) {
+    return this.http.put(this.urlArt + '/' + art.id, { 'site': art })
       .map(res => res.json());
   }
 
-  updateCategories(category){
-    return this.http.put(this.urlCat + '/' + category.id,  {'category': category})
-      .map(res => res.json());
-  }
+  deleteArt(id) {
+    return this.http.delete(this.urlArt + '/' + id)
+      .map(res => res.json());
+  }
 
-  deleteCategory(id){
-    return this.http.delete(this.urlCat + '/' + id)
-      .map(res => res.json());
-  }
+  //--------------------------------------------//
+
+  getCategories() {
+    this.http.get(this.urlCat).map((response: Response) => {
+      const cats: Category[] = response.json();
+      return cats;
+    }).subscribe(
+      (cat: Category[]) => { this.categoryService.setCategories(cat) });
+  }
+
+  getCategory(id) {
+    return this.http.get(this.urlCat + '/' + id)
+      .map(category => category.json());
+  }
+
+  addCategory(category) {
+    return this.http.post(this.urlCat, { 'category': category })
+      .map(res => res.json());
+  }
+
+  updateCategories(category) {
+    return this.http.put(this.urlCat + '/' + category.id, { 'category': category })
+      .map(res => res.json());
+  }
+
+  deleteCategory(id) {
+    return this.http.delete(this.urlCat + '/' + id)
+      .map(res => res.json());
+  }
+
+  sendEmail(toEmail: string, subjectEmail: string, messageEmail: string) {
+    // console.log(toEmail)
+    // console.log(subjectEmail)
+    // console.log(messageEmail)
+    if (toEmail && subjectEmail && messageEmail) {
+      let headers = new Headers(
+        {
+          "Content-Type": "application/x-www-form-urlencoded",
+          "Authorization": "Basic " + this.apiKey
+        }
+      );
+      let options = new RequestOptions({ headers: headers });
+      let body = "from=test@example.com&to=" + toEmail + "&subject=" + subjectEmail + "&text=" + messageEmail;
+      this.http.post("https://api.mailgun.net/v3/" + this.mailgunUrl + "/messages", body, options)
+        .map(result => result.json())
+        .do(result => console.log("RESULT: ", JSON.stringify(result)))
+        .subscribe(result => {
+          console.log("SENT!");
+          toEmail = "";
+          subjectEmail = "";
+          messageEmail = "";
+        }, error => {
+          console.log(error);
+        });
+    }
+  }
 
 }
